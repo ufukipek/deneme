1deb133f2d4350ce3cedf1b00e0f4ed0f00fce9b
Ufuk İpek <ufukipek55@hotmail.com>
2020-12-14 12:09:40 +0300

    v2

--- DenemeProjesi/Startup.cs
+++ DenemeProjesi/Startup.cs
@@ -13,6 +13,7 @@ namespace DenemeProjesi
     public class Startup
     {
         // v1
+        // v2
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
